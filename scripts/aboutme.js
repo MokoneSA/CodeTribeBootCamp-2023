124eec434d0dabf5a14e1213e5392b32713f90bb
Simon <mslephoto@gmail.com>
2023-03-01 14:41:29 +0200

    task 3

---
+++ scripts/aboutme.js
@@ -0,0 +1,32 @@
+// SUMMARY SECTION
+
+// h3 element details
+const summaryTitle = document.getElementById('summary_header').innerHTML = "Hi there!";
+
+// P element details
+const summaryParagraph = document.getElementById('summary_p').innerHTML = 
+"I'm an aspiring web developer, I'm currently looking for opportunities <br> to upskill myself. I have a basic knowledge of html and css, I believe <br> being part of this boot camp will provide me the opportunity to reach <br> new heights in my career. I am looking forward to being part of the <br> official program in the coming months.";
+
+
+// WORK EXPERIENCE SECTION 
+
+const workExpHeader = document.getElementById('work_exp_header').innerHTML = "Work Experience";
+
+// First company element
+const firstCompany = document.getElementById('first_comp').innerHTML = "Crusader Logistics";
+
+// First position element
+const firstPosition = document.getElementById('first_position').innerHTML = "Position: Receiving Supervisor, June 2018 – September 2018";
+
+
+const firstResponsibilies = document.getElementById('first_respons').innerHTML = "Responsibilies:";
+
+
+// Second company element
+const secondCompany = document.getElementById('second_comp').innerHTML = "Crusader Logistics";
+
+// Second position element
+const secondPosition = document.getElementById('second_position').innerHTML = "Position: Receiving Supervisor, June 2018 – September 2018";
+
+
+const secondResponsibilies = document.getElementById('second_respons').innerHTML = "Responsibilies:";
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const indexTitle = document.getElementById('index_main_heading').innerHTML = "Hello! I'm Simon Lephoto.";
+
+const indexParagraph = document.getElementById('index_main_paragraph').innerHTML = 
+"I have completed my diploma in information technology at Rosebank College.<br /> It majors in computer and network support, web development, security and IT<br /> project management. I'm 34 years old and live in Daveyton Etwatwa.<br />";
+
